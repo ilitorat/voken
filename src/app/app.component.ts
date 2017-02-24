d3f3e2f12966024d9dcae8322a7c427650b6bb08
lit <ilitorat@gmail.com>
2017-02-23 22:02:45 -0500

    Add login component and routing module that defaults to it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
 
+import { LoginComponent } from './login.component';
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>Hello {{name}}</h1>
+  <a routerLink="/login">Login</a>
+  <router-outlet></router-outlet>
+  `,
 })
 export class AppComponent  { name = 'World'; }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }         from '@angular/core';
+import { BrowserModule }    from '@angular/platform-browser';
+import { RouterModule }     from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { LoginComponent } from './login.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+    imports:      [
+    	BrowserModule,
+    	AppRoutingModule
+	],
+    declarations: [
+        AppComponent,
+        LoginComponent
+    ],
+    bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
