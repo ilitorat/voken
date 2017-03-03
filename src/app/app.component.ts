aea78c0c642f3597ecae49259f32c4198677b018
lit <ilitorat@gmail.com>
2017-03-02 21:52:19 -0500

    ui stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login.component';
 @Component({
   selector: 'my-app',
   template: `
+  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/angular_material/1.0.0/angular-material.min.css"/>
   <h1>V O K E N</h1>
   <router-outlet></router-outlet>
   `,
--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'my-login',
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    styleUrls: [ './login.component.css' ]
 })
 export class LoginComponent  { }
