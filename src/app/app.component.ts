2ca4cc2f881f88c7f0f51ad42bdbbac66065faab
lit <ilitorat@gmail.com>
2017-03-09 23:46:53 -0500

    pic stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,10 @@ import { LoginComponent } from './login.component';
   selector: 'my-app',
   template: `
   <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/angular_material/1.0.0/angular-material.min.css"/>
-  <h1>V O K E N</h1>
-  <router-outlet></router-outlet>
-  `,
+  <div style="background-image: url('./app/images/beach-394503_1920.jpg');top: 0px;left: 0px;position: absolute;width: 100%;height:100%">
+	  <h1 style="font-weight: 400;text-align: center;">V O K E N</h1>
+	  <router-outlet></router-outlet>
+  </div>
+  `
 })
 export class AppComponent  { name = 'World'; }
